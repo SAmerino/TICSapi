cb04958813aee18cd22d0d5ab08a50fa452c717c
SAmerino <sebastian.vicente.am@gmail.com>
2024-07-02 19:12:34 -0400

    fase 4

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: localhost,
-    user: postgres,
+    host: "localhost",
+    user: "postgres",
     password: 1234,
-    database: tics,
+    database: "tics",
     port: 5432,
 });
 
